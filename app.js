0b41f7778289850126b07de8dce11cff1afb66b5
IssaZeidan <Issa.Zeidan94@gmail.com>
2023-03-22 14:54:34 +0300

    updated

--- app.js
+++ app.js
@@ -50,8 +50,44 @@ function userGender(){
     
     alert("Your drink is being prepared")
     
+<<<<<<< Updated upstream
     answers.push(name , gender , drinkType , drinkName)
     for (let i = 0; i < answers.length; i++){
         console.log(answers[i])
     }
 }
+=======
+//     answers.push(name , gender , drinkType , drinkName)
+//     for (let i = 0; i < answers.length; i++){
+//         console.log(answers[i])
+//     }
+// }
+let username= prompt("Enter your name");
+let userGender= prompt("What is your gender male / female?");
+let age= prompt ("Enter your age");
+let drinkType= prompt("Do you want hot or cold drink?");
+let drinkName= prompt("Enter your drink name");
+
+let main= document.getElementById("main");
+
+let mainDiv= document.createElement("div");
+
+let paragraph= document.createElement("P");
+paragraph.textContent = username;
+mainDiv.appendChild(paragraph);
+
+let unOrderList= document.createElement("ul");
+let list1= document.createElement("li");
+list1.textContent=`gender: ${userGender}`;
+let list2= document.createElement("li");
+list2.textContent=`age: ${age}`;
+let list3= document.createElement("li");
+list3.textContent=`drink: ${drinkType} ${drinkName}`;
+
+unOrderList.appendChild(list1);
+unOrderList.appendChild(list2);
+unOrderList.appendChild(list3);
+
+mainDiv.appendChild(unOrderList);
+main.appendChild(mainDiv);
+>>>>>>> Stashed changes
